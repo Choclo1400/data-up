a7f1eee640e5db0b2197f04c39a6222a0a0cd7b7
Choclo1400 <114847749+Choclo1400@users.noreply.github.com>
2025-06-20 22:33:56 -0400

    Restaurar funcionalidad de modo oscuro

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AuthProvider } from './contexts/AuthContext'
+import { ThemeProvider } from 'next-themes'
 import App from './App.tsx'
+import { AuthProvider } from './contexts/AuthContext'
+import { Toaster } from '@/components/ui/sonner'
 import './index.css'
 
 const queryClient = new QueryClient({
@@ -17,12 +19,15 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <AuthProvider>
+            <App />
+            <Toaster />
+          </AuthProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
