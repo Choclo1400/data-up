fca3ad015ff898b36802f84562e16d1f03656002
Choclo1400 <114847749+Choclo1400@users.noreply.github.com>
2025-07-14 23:15:58 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './contexts/AuthContext'
 import { Toaster } from '@/components/ui/sonner';
 import { ThemeProvider } from '@/components/theme-provider';
 import { KeyboardNavigationProvider } from '@/components/accessibility/KeyboardNavigationProvider';
